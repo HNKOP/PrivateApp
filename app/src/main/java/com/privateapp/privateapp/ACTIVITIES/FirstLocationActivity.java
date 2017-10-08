a7f3388a60fd30603c2b957e126932794ae4f05f
HNKOP <emilgasanov@mail.ru>
2017-10-08 17:33:54 +0300

    Карта

--- app/src/main/java/com/privateapp/privateapp/ACTIVITIES/FirstLocationActivity.java
+++ app/src/main/java/com/privateapp/privateapp/ACTIVITIES/FirstLocationActivity.java
@@ -3,6 +3,8 @@ package com.privateapp.privateapp.ACTIVITIES;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.privateapp.privateapp.R;
 
@@ -15,6 +17,22 @@ public class FirstLocationActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_location_layout);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
 
     }
 }
